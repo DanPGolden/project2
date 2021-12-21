9cc5c313c2b0b6094096e7cb660084560498f357
Joseph Clifford <joecliffordofficial@gmail.com>
2021-12-21 12:02:18 -0800

    trying to push things

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,15 +1,75 @@
 const sequelize = require('../config/connection');
-const {Review} = require("../models/index");
+const {console, game, review, users} = require("../models");
 
 const reviewSeedData = require("./reviewSeed.json")
+const gameSeedsData = require("./gameSeeds.json")
+const consoleData = require("./console.json")
+const userData = require("./usersDummyData.json")
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
-  await Review.bulkCreate(reviewSeedData, {
+  await review.bulkCreate(reviewSeedData, {
     individualHooks: true,
     returning: true,
   });
+  await game.bulkCreate(gameSeedsData, {
+    individualHooks: true,
+    returning: true,
+  });
+  await console.bulkCreate(consoleData, {
+    individualHooks: true,
+    returning: true,
+  });
+  await users.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  })
+
   process.exit(0);
 };
 
 seedDatabase();
+
+// // Pulled from another file code stuff
+
+// const sequelize = require('../config/connection');
+// const { Traveller, Location, Trip } = require('../models');
+
+// const travellerSeedData = require('./travellerSeedData.json');
+// const locationSeedData = require('./locationSeedData.json');
+
+// const seedDatabase = async () => {
+//   await sequelize.sync({ force: true });
+
+//   const travellers = await Traveller.bulkCreate(travellerSeedData);
+
+//   const locations = await Location.bulkCreate(locationSeedData);
+
+//   // Create trips at random
+//   for (let i = 0; i < 10; i++) {
+//     // Get a random traveller's `id`
+//     const { id: randomTravellerId } = travellers[
+//       Math.floor(Math.random() * travellers.length)
+//     ];
+
+//     // Get a random location's `id`
+//     const { id: randomLocationId } = locations[
+//       Math.floor(Math.random() * locations.length)
+//     ];
+
+//     // Create a new trip with random `trip_budget` and `traveller_amount` values, but with ids selected above
+//     await Trip.create({
+//       trip_budget: (Math.random() * 10000 + 1000).toFixed(2),
+//       traveller_amount: Math.floor(Math.random() * 10) + 1,
+//       traveller_id: randomTravellerId,
+//       location_id: randomLocationId
+//     }).catch((err) => {
+//       // If there's an error, such as the same random pairing of `traveller.id` and `location.id` occurring and we get a constraint error, don't quit the Node process
+//       console.log(err);
+//     });
+//   }
+
+//   process.exit(0);
+// };
+
+// seedDatabase();
